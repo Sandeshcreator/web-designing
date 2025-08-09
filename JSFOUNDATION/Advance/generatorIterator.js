725a31cb833f56aada4b9454423e7929382540a4
Sandeshcreator <mainalisandesh8@gmail.com>
2025-08-09 18:57:32 +0545

    Iterator and es6 and commonJs

---
+++ JSFOUNDATION/Advance/generatorIterator.js
@@ -0,0 +1,20 @@
+const { some } = require("micromatch");
+
+function* numberGenerator() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+let gen = numberGenerator();
+let genTwo = numberGenerator();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+
+console.log(genTwo.next().value);
+console.log(genTwo.next().value);
+console.log(genTwo.next().value);
+console.log(genTwo.next().value);
